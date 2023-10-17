bf616648f4dc29a93cc10ae49675fccffe550e51
h4tr <traugottolsen@live.dk>
2023-10-17 15:12:12 +0200

    15 POINT QUERY ADDED

--- hackerrankSQL.sql
+++ hackerrankSQL.sql
@@ -129,3 +129,25 @@ FROM
 WHERE  
     CITY RLIKE '[aA-zZ].*[^aeiouAEIOU]$'
 ;
+
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT
+    CITY
+FROM
+    STATION
+WHERE  
+    CITY RLIKE '^[^aeiouAEIOU].*|[^aeiouAEIOU]$'
+;
+
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT
+    CITY
+FROM
+    STATION
+WHERE  
+    CITY RLIKE '^[^aeiouAEIOU].*[^aeiouAEIOU]$'
+;
